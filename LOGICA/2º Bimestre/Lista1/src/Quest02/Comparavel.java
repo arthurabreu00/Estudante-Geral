879675d4f6083b43c1a220e955599f26d4c3f38d
Arthur Flávio de Abreu Tavares <277093@UTRAMIGBH.SEDE>
2018-10-02 22:26:31 -0300

    Aula 02-10-2018

---
+++ LOGICA/2º Bimestre/Lista1/src/Quest02/Comparavel.java
@@ -0,0 +1,33 @@
+package Quest02;
+
+public class Comparavel {
+
+	/*
+	 * 1. ESCREVA UMA CLASSE CHAMADA “COMPARAVEL” QUE TEM COMO ATRIBUTO UM VALOR
+	 * INTEIRO, UM CONSTRUTOR PARA INICIALIZAR O ATRIBUTO E OS MÉTODOS
+	 * EHMAIOROUIGUAL, EHMENOROUIGUAL E EHDIFERENTEDE QUE RECEBEM UM VALOR DO
+	 * TIPO INT COMO PARÂMETRO E RETORNAM TRUE SE O VALOR ENCAPSULADO FOR,
+	 * RESPECTIVAMENTE, MAIOR OU IGUAL, MENOR OU IGUAL OU DIFERENTE DO PASSADO
+	 * COMO PARÂMETRO. FAÇA UMA CLASSE TESTACOMPARAVEL QUE VOCÊ POSSA DIGITAR UM
+	 * NÚMERO E UTILIZAR OS MÉTODOS.
+	 */
+
+	int valor;
+
+	public Comparavel(int x) {
+
+		this.valor = x;
+
+	}
+	
+	public boolean ehMaiorOuIgual(int x){
+		
+		if(this.valor  >= x)
+			return true;
+		return false;
+		
+	}
+	
+
+
+}
---
+++ LOGICA/2º Bimestre/Lista1/src/Quest02/TestaComparavel.java
@@ -0,0 +1,20 @@
+package Quest02;
+
+import java.util.Scanner;
+
+public class TestaComparavel {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite um número: ");
+		int num = input.nextInt();
+		
+		Comparavel comp = new Comparavel(num);
+		
+		
+				
+	}
+
+}
